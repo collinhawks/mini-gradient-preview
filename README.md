c207013f07b3d1cf5e85a7dc12c3c2620730f42f
VigneshR <vickylab2000@gmail.com>
2023-01-19 17:05:47 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Mini Gradient Preview 🌈
+Mini Gradient Preview allows you to visualize your css gradients in the editor in the form of mini previews next to the gradient declaration
+
+## Features
+
+* Works on `linear-gradient`,`radial-gradient`,`conical-gradient`,`repeating-linear-gradient`.
+
+!["first.png"](./images/1.png)
+
+* Works on multi-line declarations.
+
+!["second.png"](./images/2.png)
+
+* Real-time changes.
+
+!["third.gif"](./images/3.gif)
+
+* Show or Hide previews in your workspace using `Show Gradient Previews` and `Hide Gradient Previews` commands.
+
+!["third.gif"](./images/4.gif)
+
+* Works on all types of files that have css gradient declarations
+
+!["third.gif"](./images/5.png)
+
+* Only self contained CSS gradients are supported (no variables).
+
+
+Icon provided by [Murphy](https://www.linkedin.com/in/murphycaxton/)
+## Release Notes
+
+
+### 1.0.0
+
+Initial release of mini gradient preview
+
+
+
---
+++ src/extension.ts
@@ -0,0 +1,163 @@
+import * as vscode from 'vscode';
+
+
+
+//Event handles
+let textEditorEvent:vscode.Disposable | null = null;
+let textDocEvent:vscode.Disposable | null = null;
+
+//Constants
+const WORKSPACE_SECTION = "mini-gradient-preview"
+const noDecorationType:vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType({});
+
+
+
+//returns dynamic decoration based on font size
+function getDecoration(type:string,value:string):vscode.DecorationInstanceRenderOptions{
+  const fontSize = parseFloat(vscode.workspace.getConfiguration().get('editor.fontSize') as string);
+  let width:number = 50.0 * (fontSize/14)
+  let bottom:number = 4 * (fontSize/14)
+  return {
+    before:{
+      contentText:"",
+      height: "90%",
+      width: `${width}px`,
+      margin: `0px 4px -${bottom}px 1px`,
+      backgroundColor: `background-color: transparent; background-image: ${type+value};`,
+      border: "1px solid white"
+    }
+  }
+
+}
+
+//Helper functions
+function addDecorations(){
+  const editor = vscode.window.activeTextEditor;
+  if(editor)
+    decorate(editor); 
+}
+
+function removeDecorations(){
+  const editor = vscode.window.activeTextEditor;
+  if(editor)
+    editor.setDecorations(noDecorationType,[]);
+}
+
+function showGradients(){
+  hideGradients();
+  addDecorations();
+  textEditorEvent = vscode.window.onDidChangeActiveTextEditor(e => {
+    addDecorations();
+  });
+	textDocEvent = vscode.workspace.onDidChangeTextDocument(e => {
+	addDecorations();
+  });
+
+}
+
+function hideGradients(){
+  if(textEditorEvent != null){
+    textEditorEvent.dispose();
+    textEditorEvent = null;
+  }
+  if(textDocEvent != null){
+    textDocEvent.dispose()
+    textDocEvent = null;
+  }
+  removeDecorations();
+}
+
+function changeConfiguration(visibility:boolean){
+  const config = vscode.workspace.getConfiguration(WORKSPACE_SECTION);
+  config.update("show",visibility,vscode.ConfigurationTarget.Workspace).then();
+}
+
+function activate()  {
+  //Program entry point
+  // Loads workspace configurations
+  const config = vscode.workspace.getConfiguration(WORKSPACE_SECTION);
+
+  //Previews are shown based on configuration 
+  if(config.get("show")){
+    showGradients();
+  }
+
+  //sets up configuration and command handlers
+  vscode.workspace.onDidChangeConfiguration(e => {  
+    if(e.affectsConfiguration(WORKSPACE_SECTION)){  
+      const visibility = vscode.workspace.getConfiguration(WORKSPACE_SECTION).get("show"); 
+      if (visibility){
+        showGradients();
+      }
+      else{
+        hideGradients();
+      }
+    }
+  
+    if (e.affectsConfiguration('editor.fontSize')) {
+      const visibility = vscode.workspace.getConfiguration(WORKSPACE_SECTION).get("show");
+      if(visibility){
+        addDecorations();
+      }
+    }
+  });
+  vscode.commands.registerCommand('gradientpreview.showgradientpreviews', () => {
+    changeConfiguration(true);
+  });
+  vscode.commands.registerCommand('gradientpreview.hidegradientpreviews', () => {
+    changeConfiguration(false);
+  });
+}
+
+
+function decorate(editor:vscode.TextEditor) {
+  //Uses regex to find matches in each line
+  //Uses bracket balancing algorithm to extract gradient value
+  let sourceCode:string = editor.document.getText();
+  let regex:RegExp = /(linear-gradient|radial-gradient|conic-gradient|repeating-linear-gradient)\(/g;
+  const sourceCodeArr:string[] = sourceCode.split("\n");
+  let decorations:vscode.DecorationOptions[] = [];
+
+  for(let line=0;line<sourceCodeArr.length;line++){
+
+    let matches = [...sourceCodeArr[line].matchAll(regex)]
+    for(let match=0;match<matches.length;match++){
+      let value:string = "("; 
+      let subline:number = line;
+      const stack:string[] = []
+      stack.push("(")
+      for(;subline<sourceCodeArr.length && stack.length > 0;subline++){
+        let i = 0;
+        if (subline === line){
+          i = matches[match].index as number + matches[match][0].length
+        }
+        for(;i<sourceCodeArr[subline].length && stack.length > 0;i++){
+          if (sourceCodeArr[subline][i] === "(")
+            stack.push("(")
+          else if (sourceCodeArr[subline][i] === ")")
+            stack.pop()
+          value += sourceCodeArr[subline][i]
+        }
+      }
+	//Adds the preview to the decorations array
+      let range:vscode.Range = new vscode.Range(
+        new vscode.Position(line, matches[match].index as number),
+        new vscode.Position(line, matches[match].index as number)
+      );
+
+      decorations.push({
+        range:range,
+        renderOptions:getDecoration(matches[match][1],value)
+      })
+      
+    }
+  }
+  //Shows the preview in editor
+  editor.setDecorations(noDecorationType,decorations);
+
+
+}
+
+module.exports = {
+	activate
+}
